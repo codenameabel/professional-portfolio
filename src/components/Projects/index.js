4fa607367a8b7a074e36d31430d31b27d0fe08e9
codenameabel <newabelmartinez@gmail.com>
2021-08-23 10:17:51 -0700

    cleaning branch before starting new code

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -45,7 +45,7 @@ function Projects() {
                 <div className="row pb-0 pe-lg-0 pt-lg-5 align-items-center rounded-3 border">
                     <div className="col-lg-7 p-3 mx-lg-6 p-lg-5 pt-lg-3">
                         <h1 className="display-4 fw-bold lh-1" style={{ color: '#004e89' }}>Project Aqua Nauticus</h1>
-                        <p className="lead" style={{ color: '#1a659e' }}>The PAN application is designed to provide weather information about a city's coastline. Our goal is to provide robust data to environmentalists, marine biologists, and those who are concerned about the wellness of their marine environment.</p>
+                        <p className="lead" style={{ color: '#1a659e' }}>PAN or Project Aqua Nauticus is a weather application that keeps track of weather conditions and gives you recent enviromental news about the searched city. This project was done with two class mates during bootcamp.</p>
                         <div className="d-grid gap-2 d-md-flex justify-content-md-start mb-4 mb-lg-3">
                             <a href="https://patricklago21.github.io/ucberk-project-1/" target="_blank" rel="noopener noreferrer" type="button" className="btn btn-lg px-4 me-md-2 fw-bold" style={{ background: '#ff6b35' }}><span style={{ color: '#ffffff' }}>Check It Out</span></a>
                         </div>
